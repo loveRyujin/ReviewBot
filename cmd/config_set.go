be5c5b731eff5ace7072fc432f19357e7a84dda2
lichen <1725350830@qq.com>
2025-05-06 17:57:57 +0800

    feat: 实现config set命令，更新配置文件

--- cmd/config_set.go
+++ cmd/config_set.go
@@ -1,6 +1,13 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"errors"
+	"strings"
+
+	"github.com/fatih/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
 
 func init() {
 	configCmd.AddCommand(configSetCmd)
@@ -8,8 +15,28 @@ func init() {
 
 var configSetCmd = &cobra.Command{
 	Use:   "set",
-	Short: "Set a configuration value",
+	Short: "Set configuration value",
+	Args:  cobra.MinimumNArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
+		// check if the key is valid
+		if _, exist := availableKeys[args[0]]; !exist {
+			return errors.New("invalid config key, please use 'reviewbot config list' to see all available keys")
+		}
+
+		// set the config value in viper
+		if args[0] == "git.exclude_list" {
+			viper.Set(args[0], strings.Split(args[1], ","))
+		} else {
+			viper.Set(args[0], args[1])
+		}
+
+		// write config to file
+		if err := viper.WriteConfig(); err != nil {
+			return err
+		}
+
+		color.Green("set config value successfully, you can see the config file: %s", viper.ConfigFileUsed())
+
 		return nil
 	},
 }
