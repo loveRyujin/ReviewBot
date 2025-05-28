45b875cd44d72d05b3c927cfeebc92d65c883a0d
lichen <1725350830@qq.com>
2025-05-28 14:54:44 +0800

    feat：proxy配置支持添加请求头

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -5,10 +5,50 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 
 	"golang.org/x/net/proxy"
 )
+
+type customTransport struct {
+	transport http.RoundTripper
+	headers   http.Header
+}
+
+// RoundTrip adds custom headers and forwards HTTP requests
+func (c *customTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	if c.transport == nil {
+		return nil, fmt.Errorf("origin transport is not set")
+	}
+
+	// Set custom headers
+	for key, values := range c.headers {
+		for _, value := range values {
+			req.Header.Add(key, value)
+		}
+	}
+
+	return c.transport.RoundTrip(req)
+}
+
+// toHTTPHeaders converts a slice of strings in the format "Key=Value" to http.Header.
+func toHTTPHeaders(headers []string) http.Header {
+	httpHeaders := make(http.Header)
+	for _, header := range headers {
+		parts := strings.SplitN(header, "=", 2)
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			if key == "" || value == "" {
+				continue // Skip empty keys or values
+			}
+			httpHeaders.Add(key, value)
+		}
+	}
+
+	return httpHeaders
+}
 
 type Config struct {
 	ProxyURL   string
@@ -26,12 +66,6 @@ func (cfg *Config) New() (*http.Client, error) {
 		},
 	}
 
-	// Create a custom HTTP client with the specified timeout and transport.
-	httpClient := &http.Client{
-		Timeout:   cfg.Timeout,
-		Transport: transport,
-	}
-
 	// Configure proxy settings if provided.
 	if cfg.ProxyURL != "" {
 		proxyURL, err := url.Parse(cfg.ProxyURL)
@@ -46,5 +80,15 @@ func (cfg *Config) New() (*http.Client, error) {
 		}
 		transport.DialContext = dialer.(proxy.ContextDialer).DialContext
 	}
+
+	// Create the HTTP client with the custom transport and headers.
+	httpClient := &http.Client{
+		Timeout: cfg.Timeout,
+		Transport: &customTransport{
+			transport: transport,
+			headers:   toHTTPHeaders(cfg.Headers),
+		},
+	}
+
 	return httpClient, nil
 }
